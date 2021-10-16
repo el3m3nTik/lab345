6fc13ad4f1a9a8a4fc6efb07527a195b537348fe
el3m3nTik <90087363+el3m3nTik@users.noreply.github.com>
2021-10-16 10:01:45 +0200

    Update lab4_3.cpp
    Нормальные названия

--- lab4_3.cpp
+++ lab4_3.cpp
@@ -4,30 +4,30 @@
 using namespace std;
 int main()
 {
-    const int ROW = 4;
-    const int COL = 3;
+    const int ROW = 3;
+    const int COL = 4;
     int sum = 10000;
     int index = 0;
-    int matrix[COL][ROW] =
+    int matrix[ROW][COL] =
     {
     {43,10,10,1},
     {12,28,99,34},
     {2,50,4,5}
     };
-    for (int i = 0, temp = 0; i < ROW; i++) {
-        
-        for (int j = 0; j < COL; j++) {
-            
-            temp += matrix[j][i];
+    for (int i = 0, temp = 0; i < COL; i++) {
+
+        for (int j = 0; j < ROW; j++) {
+
+            temp += matrix[i][j];
         }
         if (sum > temp) {
             sum = temp;
             index = i;
         }
-        
+
         temp = 0;
     }
-    for (int i = 0; i < COL + 1; i++) {
+    for (int i = 0; i < COL; i++) {
         matrix[i][index] *= 3;
     }
 
